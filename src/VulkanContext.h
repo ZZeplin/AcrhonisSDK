83f7f368f4c7e26fd2a684a3dfbde4399a5ffee1
Logan OConnor <loganoconnor50@gmail.com>
2026-07-25 23:56:42 -0500

    Added and updated code

--- src/VulkanContext.h
+++ src/VulkanContext.h
@@ -6,16 +6,31 @@
 
 #include <src/Window.h>
 #include <Vulkan/vulkan.h>
+#include <optional>
+#include <vector>
 
-struct VulkanContext
-{
+struct VulkanContext {
 	VkInstance instance;
 	VkSurfaceKHR surface;
 	VkPhysicalDevice physicalDevice;
 	VkDevice device;
+	VkQueue graphicsQueue;
 };
 
-bool vkInstance(VulkanContext& context, GLFWwindow* window);
+struct QueueFamilyIndices {
+	std::optional<uint32_t> graphicsFamily;
+};
+
+QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+
+bool createInstance(VulkanContext& context, GLFWwindow* window);
 bool vkSurfaceKHR(VulkanContext& context, GLFWwindow* window);
-bool vkPhysicalDevice(VulkanContext& context, GLFWwindow* window);
-bool vkDevice(VulkanContext& context, GLFWwindow* window);
+bool vkPickPhysicalDevice(VulkanContext& context, GLFWwindow* window);
+bool isDeviceSuitable(VkPhysicalDevice device);
+bool isQueueFamilyComplete(const QueueFamilyIndices& indices);
+bool checkValidationLayerSupport(VulkanContext& context, GLFWwindow* winodw);
+
+void createLogicalDevice(VulkanContext& context, GLFWwindow* window);
+void cleanup(VulkanContext& context, GLFWwindow* window);
+
+std::vector<const char*> getRequiredExtensions();
--- src/main.cpp
+++ src/main.cpp
@@ -10,57 +10,22 @@
 
 #include <vector>
 
-// TODO: physicalDevice and device are delcated but not created.
-//	 Need physical device selection (vkEnumeratePhysicalDevices +
-//	 suitability check_ and logical device creation before this
-//	 context is usable for rendering. Also, cleanup VulkanContext. Yays
-
-int main()
-{
+int main() {
 	WindowConfig window{};
 	WindowSystem::Initialize(window);
 
-	if (!window.handle) 
-	{
+	if (!window.handle) {
 		return -1;
 	}
 
 	VulkanContext context{};
-
-	if (!vkInstance(context, window.handle))
-	{
-		std::cerr << "VULKAN INSTANCE FAILED AT RUNTIME" << std::endl;
-		return -1;
-	}
-
-	if (!vkSurfaceKHR(context, window.handle))
-	{
-		std::cerr << "VULKAN SURFACE FAILED AT RUNTIME" << std::endl;
-		return -1;
-	}
-
-	if (!vkPhysicalDevice(context, window.handle))
-	{
-		std::cerr << "VULKAN PHYSICALDEVICE FAILED AT RUNTIME" << std::endl;
-		return -1;
-	}
-
-	if (!vkDevice(context, window.handle))
-	{
-		std::cerr << "VULKAN DEVICE FAILED AT RUNTIME" << std::endl;
-		return -1;
-	}
 	
 	std::cout << "SUCCESS WINDOW AND INSTANCE WERE CREATED" << std::endl;
 
-	while (!window.shouldClose) 
-	{
+	while (!window.shouldClose) {
 		WindowSystem::Update(window);
 	}
 
-	vkDestroyDevice(context.device, nullptr);
-	vkDestroySurfaceKHR(context.instance, context.surface, nullptr);
-	vkDestroyInstance(context.instance, nullptr);
 	WindowSystem::Shutdown(window);
 
 	return 0;
